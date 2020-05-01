fe209d6f661f4a40c67200a82f4d8c776bc211d2
crypto-hackathons <63258586+crypto-hackathons@users.noreply.github.com>
2020-05-01 17:08:49 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,21 +4,35 @@
 export PROJECT_USER=$1
 export INSTALL_DIR=`pwd`
 export PROJECT_CONF=$INSTALL_DIR/elementsProject
+
+echo "**************"
+echo "Params"
+echo "**************"
+echo "PROJECT_USER = $PROJECT_USER"
+echo "INSTALL_DIR = $INSTALL_DIR"
+echo "PROJECT_CONF = $PROJECT_CONF"
+
 source $PROJECT_CONF
 
-# Required
+echo "**************"
+echo "Apt install"
+echo "**************"
 apt-get update && apt-get upgrade
 apt-get install git build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler imagemagick librsvg2-bin libqrencode-dev autoconf openssl libssl-dev libevent-dev libminiupnpc-dev jq  haskell-platform xz-utils autotools-dev automake g++ gpp pkg-config libdb++-dev libboost-all-dev libncurses-dev make
 apt-get update --fix-missing
 
-# Berkeley DB
+echo "**************"
+echo "Berkeley DB"
+echo "**************"
 cd $PROJECT_DIR
 git clone https://github.com/ElementsProject/elements.git
 chmod 0755 $PROJECT_ELEMENTS_DIR && chown PROJECT_USER $PROJECT_BITCOIN_DIR
 cd $PROJECT_ELEMENTS_DIR
 ./contrib/install_db4.sh $DB4_INSTALL_PATH
 
-# Bitcoin
+echo "**************"
+echo "Bitcoin"
+echo "**************"
 cd $PROJECT_DIR
 git clone https://github.com/roconnor-blockstream/bitcoin.git
 chmod 0755 $PROJECT_BITCOIN_DIR && chown PROJECT_USER $PROJECT_BITCOIN_DIR
@@ -29,10 +43,18 @@ git checkout simplicity
 make
 cd $PROJECT_BITCOIN_DIR
 alias btc="$PROJECT_BITCOIN_DIR/src/./bitcoin-cli -regtest"
+
+echo "btc = $PROJECT_BITCOIN_DIR/src/./bitcoin-cli -regtest"
+
 alias btcd="$PROJECT_BITCOIN_DIR/src/./bitcoind -regtest"
+
+echo "btcd = $PROJECT_BITCOIN_DIR/src/./bitcoind -regtest"
+
 btcd -daemon
 
-# Simplicity
+echo "**************"
+echo "Simplicity"
+echo "**************"
 cd $PROJECT_DIR
 git clone https://github.com/ElementsProject/simplicity.git
 chmod 0755 $PROJECT_SIMPLICITY_DIR && chown PROJECT_USER $PROJECT_SIMPLICITY
@@ -54,9 +76,14 @@ curl https://nixos.org/nix/install | sh
 . /home/$PROJECT_USER/.nix-profile/etc/profile.d/nix.sh
 cd $PROJECT_SIMPLICITY_DIR 
 nix-shell -p "(import ./default.nix {}).haskellPackages.ghcWithPackages (pkgs: with pkgs; [Simplicity bech32])"
-alias simplicityenv="cd $PROJECT_SIMPLICITY_DIR && nix-shell -p \"(import ./default.nix {}).haskellPackages.ghcWithPackages (pkgs: with pkgs; [Simplicity bech32])\"
 
-# Elements
+alias simplicityenv="cd $PROJECT_SIMPLICITY_DIR && nix-shell -p \"(import ./default.nix {}).haskellPackages.ghcWithPackages (pkgs: with pkgs; [Simplicity bech32])\""
+
+echo "simplicityenv = cd $PROJECT_SIMPLICITY_DIR && nix-shell -p \"(import ./default.nix {}).haskellPackages.ghcWithPackages (pkgs: with pkgs; [Simplicity bech32])\""
+
+echo "**************"
+echo "Elements"
+echo "**************"
 cd $PROJECT_ELEMENTS_DIR
 ./autogen.sh
 ./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --disable-dependency-tracking --with-gui=no --disable-test --disable-bench
@@ -64,7 +91,9 @@ make
 make install
 which elementsd
 
-# Personas
+echo "**************"
+echo "Personas"
+echo "**************"
 cd $PROJECT_ELMENTS_DIR/contrib/assets_tutorial
 mkdir $USER_BITCOIN_DIR
 cp ./bitcoin.conf $BITCOIN_DIR/bitcoin.conf
@@ -73,20 +102,33 @@ cp ./elements1.conf $USER_ALICE_DIR/elements.conf
 mkdir $USER_BOB_DIR
 cp ./elements2.conf $USER_BOB_DIR/elements.conf
 
-# start deamons
+echo "**************"
+echo "Start deamons"
+echo "**************"
 b-dae
 alice-dae
 bob-dae
 
-# Personas address creation
+echo "**************"
+echo "Personas address"
+echo "**************"
 ALICE_MINER_ADDRESS=$(alice-cli getnewaddress)
 echo "ALICE_MINER_ADDRESS=$ALICE_MINER_ADDRESS" >> $PROJECT_CONF
+echo "echo \"ALICE_MINER_ADDRESS = \$ALICE_MINER_ADDRESS\"" >> $PROJECT_CONF
 ALICE_RECEIVER_ADDRESS=$(alice-cli getnewaddress)
 echo "ALICE_RECEIVER_ADDRESS=$ALICE_RECEIVER_ADDRESS" >> $PROJECT_CONF
+echo "echo \echo "ALICE_RECEIVER_ADDRESS = \$ALICE_RECEIVER_ADDRESS\"" >> $PROJECT_CONF
 BOB_RECEIVER_ADDRESS=$(bob-cli getnewaddress)
 echo "BOB_RECEIVER_ADDRESS=$BOB_RECEIVER_ADDRESS" >> $PROJECT_CONF
+echo "echo \echo "BOB_RECEIVER_ADDRESS = \$BOB_RECEIVER_ADDRESS\"" >> $PROJECT_CONF
 
+echo "ALICE_MINER_ADDRESS = $ALICE_MINER_ADDRESS"
+echo "ALICE_RECEIVER_ADDRESS = $ALICE_RECEIVER_ADDRESS"
+echo "BOB_RECEIVER_ADDRESS = $BOB_RECEIVER_ADDRESS"
+
+echo "**************"
+echo "chmod +x"
+echo "**************"
 chmod +x elementsProjectStart.sh
 chmod +x elementsProjectStop.sh
 chmod +x test_transaction_simple.sh  
-
