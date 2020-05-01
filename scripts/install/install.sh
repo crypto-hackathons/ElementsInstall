9e34477ca8288afc1b5d94a9bfe1c9e65c011cde
crypto-hackathons <63258586+crypto-hackathons@users.noreply.github.com>
2020-05-01 22:58:32 +0200

    Add files via upload

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -24,13 +24,12 @@ chmod 0755 $PROJECT_DIR && chown $PROJECT_USER $PROJECT_DIR
 echo "**************"
 echo "Apt install"
 echo "**************"
-
 echo "apt-get update >> $INSTALL_LOG_DIR/apt_update.log"
 apt-get update  >> $INSTALL_LOG_DIR/apt_update.log
 echo "apt-get upgrade >> $INSTALL_LOG_DIR/apt_upgrade.log"
 apt-get upgrade >> $INSTALL_LOG_DIR/apt_upgrade.log
 
-echo "apt-get install git build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler imagemagick librsvg2-bin libqrencode-dev autoconf openssl libssl-dev libevent-dev libminiupnpc-dev jq haskell-platform xz-utils autotools-dev automake g++ gpp pkg-config libdb++-dev libboost-all-dev libncurses-dev make >> $INSTALL_LOG_DIR/apt.log"
+echo "apt-get install git apt-utils build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler imagemagick librsvg2-bin libqrencode-dev autoconf openssl libssl-dev libevent-dev libminiupnpc-dev jq haskell-platform xz-utils autotools-dev automake g++ gpp pkg-config libdb++-dev libboost-all-dev libncurses-dev make >> $INSTALL_LOG_DIR/apt.log"
 apt-get install git build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler imagemagick librsvg2-bin libqrencode-dev autoconf openssl libssl-dev libevent-dev libminiupnpc-dev jq haskell-platform xz-utils autotools-dev automake g++ gpp pkg-config libdb++-dev libboost-all-dev libncurses-dev make >> $INSTALL_LOG_DIR/apt.log
 
 echo "apt-get update --fix-missing >> $INSTALL_LOG_DIR/apt_missing.lo"
