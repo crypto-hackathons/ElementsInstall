e566328fc74391816e722d66e2846612b063dc3a
crypto-hackathons <63258586+crypto-hackathons@users.noreply.github.com>
2020-05-01 20:24:21 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -41,8 +41,8 @@ git clone https://github.com/roconnor-blockstream/bitcoin.git
 chmod 0755 $PROJECT_BITCOIN_DIR && chown $PROJECT_USER $PROJECT_BITCOIN_DIR
 cd $PROJECT_BITCOIN_DIR
 git checkout simplicity
-./autogen.sh
-./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --disable-dependency-tracking --with-gui=no --disable-test --disable-bench
+su  $PROJECT_USER -c ./autogen.sh
+su  $PROJECT_USER -c ./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --disable-dependency-tracking --with-gui=no --disable-test --disable-bench
 make
 cd $PROJECT_BITCOIN_DIR
 
@@ -60,24 +60,24 @@ echo "**************"
 cd $PROJECT_DIR
 git clone https://github.com/ElementsProject/simplicity.git
 chmod 0755 $PROJECT_SIMPLICITY_DIR && chown $PROJECT_USER $PROJECT_SIMPLICITY
-cabal update
-cabal install bech32-1.0.2
-cabal install unification-fd cereal lens-family-2.0.0 SHA MemoTrie
+su  $PROJECT_USER -c cabal update
+su  $PROJECT_USER -c cabal install bech32-1.0.2
+su  $PROJECT_USER -c cabal install unification-fd cereal lens-family-2.0.0 SHA MemoTrie
 cd $PROJECT_SIMPLICITY_DIR
 git checkout 2867955c0c93418f45ffe8ea0a7b1277b785fdc4
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source $HOME/.cargo/env
+su  $PROJECT_USER -c curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+su  $PROJECT_USER -c source $HOME/.cargo/env
 mkdir $PROJECT_HAL_DIR
 chmod 0755 $PROJECT_HAL_DIR && chown $PROJECT_USER $PROJECT_HAL_DIR
 cd $PROJECT_HAL_DIR
 wget https://github.com/stevenroose/hal/releases/download/v0.6.1/hal-0.6.1-vendored.tar.gz
-tar xzf hal-0.6.1-vendored.tar.gz
-cargo install hal
+su  $PROJECT_USER -c tar xzf hal-0.6.1-vendored.tar.gz
+su  $PROJECT_USER -c cargo install hal
 mkdir -m 0755 /nix && chown $PROJECT_USER /nix
-curl https://nixos.org/nix/install | sh
-. /home/$PROJECT_USER/.nix-profile/etc/profile.d/nix.sh
+su  $PROJECT_USER -c curl https://nixos.org/nix/install | sh
+su  $PROJECT_USER -c . /home/$PROJECT_USER/.nix-profile/etc/profile.d/nix.sh
 cd $PROJECT_SIMPLICITY_DIR 
-nix-shell -p "(import ./default.nix {}).haskellPackages.ghcWithPackages (pkgs: with pkgs; [Simplicity bech32])"
+su  $PROJECT_USER -c nix-shell -p "(import ./default.nix {}).haskellPackages.ghcWithPackages (pkgs: with pkgs; [Simplicity bech32])"
 
 alias simplicityenv="cd $PROJECT_SIMPLICITY_DIR && nix-shell -p \"(import ./default.nix {}).haskellPackages.ghcWithPackages (pkgs: with pkgs; [Simplicity bech32])\""
 echo "simplicityenv = cd $PROJECT_SIMPLICITY_DIR && nix-shell -p \"(import ./default.nix {}).haskellPackages.ghcWithPackages (pkgs: with pkgs; [Simplicity bech32])\""
@@ -86,8 +86,8 @@ echo "**************"
 echo "Elements"
 echo "**************"
 cd $PROJECT_ELEMENTS_DIR
-./autogen.sh
-./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --disable-dependency-tracking --with-gui=no --disable-test --disable-bench
+su  $PROJECT_USER -c ./autogen.sh
+su  $PROJECT_USER -c ./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --disable-dependency-tracking --with-gui=no --disable-test --disable-bench
 make
 make install
 which elementsd
@@ -96,11 +96,17 @@ echo "**************"
 echo "Personas"
 echo "**************"
 cd $PROJECT_ELMENTS_DIR/contrib/assets_tutorial
+
 mkdir $USER_BITCOIN_DIR
+chmod 0755 $USER_BITCOIN_DIR && chown $PROJECT_USER $USER_BITCOIN_DIR
 cp ./bitcoin.conf $BITCOIN_DIR/bitcoin.conf
+
 mkdir $USER_ALICE_DIR
+chmod 0755 $USER_ALICE_DIR && chown $PROJECT_USER $USER_ALICE_DIR
 cp ./elements1.conf $USER_ALICE_DIR/elements.conf
+
 mkdir $USER_BOB_DIR
+chmod 0755 $USER_BOB_DIR && chown $PROJECT_USER $USER_BOB_DIR
 cp ./elements2.conf $USER_BOB_DIR/elements.conf
 
 echo "**************"
@@ -113,15 +119,15 @@ bob-dae
 echo "**************"
 echo "Personas address"
 echo "**************"
-ALICE_MINER_ADDRESS=$(alice-cli getnewaddress)
+su  $PROJECT_USER -c ALICE_MINER_ADDRESS=$(alice-cli getnewaddress)
 echo "ALICE_MINER_ADDRESS=$ALICE_MINER_ADDRESS" >> $PROJECT_CONF
 echo "echo \"ALICE_MINER_ADDRESS = \$ALICE_MINER_ADDRESS\"" >> $PROJECT_CONF
 
-ALICE_RECEIVER_ADDRESS=$(alice-cli getnewaddress)
+su  $PROJECT_USER -c ALICE_RECEIVER_ADDRESS=$(alice-cli getnewaddress)
 echo "ALICE_RECEIVER_ADDRESS=$ALICE_RECEIVER_ADDRESS" >> $PROJECT_CONF
 echo "echo \echo "ALICE_RECEIVER_ADDRESS = \$ALICE_RECEIVER_ADDRESS\"" >> $PROJECT_CONF
 
-BOB_RECEIVER_ADDRESS=$(bob-cli getnewaddress)
+su  $PROJECT_USER -c BOB_RECEIVER_ADDRESS=$(bob-cli getnewaddress)
 echo "BOB_RECEIVER_ADDRESS=$BOB_RECEIVER_ADDRESS" >> $PROJECT_CONF
 echo "echo \echo "BOB_RECEIVER_ADDRESS = \$BOB_RECEIVER_ADDRESS\"" >> $PROJECT_CONF
 
