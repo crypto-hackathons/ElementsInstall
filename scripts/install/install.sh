8c3da9beda05536d6e94a114e98f9f7339053d68
crypto-hackathons <63258586+crypto-hackathons@users.noreply.github.com>
2020-05-03 23:03:44 +0200

    Add files via upload

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -34,12 +34,13 @@ allCd $1 "$PROJECT_DIR"
 userGitClone $1 "https://github.com/roconnor-blockstream/bitcoin.git" "bitcoin_gitClone" "$PROJECT_BITCOIN_DIR"
 allCd $1 "$PROJECT_BITCOIN_DIR"
 userGitChekout $1 "simplicity"
+
+exit
 userDoLog $1 "./autogen.sh" "bitcoin_autogen"
 userDoLog $1 "./configure BDB_LIBS=\"-L${BDB_PREFIX}/lib -ldb_cxx-4.8\" BDB_CFLAGS=\"-I${BDB_PREFIX}/include\" --disable-dependency-tracking --with-gui=no --disable-test --disable-bench --disable-jni" "bitcoin_configure"
 suDo "make" "bitcoin_make"
 allCd $1 "$PROJECT_BITCOIN_DIR"
 suDo "btcd -daemon"
-exit
 
 echo "**************"
 echo "Simplicity"
@@ -66,126 +67,78 @@ userWgetTarxzf $1 "https://github.com/stevenroose/hal/releases/download/v0.6.1/h
 
 userDoLog $1 "cargo install hal" "hal_install"
 
-if [ -d "/nix" ]
-then
-    echo "/nix exist, rm"
-    rm -rf /nix
-fi
+userMkdir $1 "$PROJECT_NIX_DIR"
 
-echo "mkdir -m 0755 /nix && chown $PROJECT_USER /nix"
-mkdir -m 0755 /nix && chown $PROJECT_USER /nix
+userDo $1 "curl https://nixos.org/nix/install | sh"
 
-echo "userDo \"curl https://nixos.org/nix/install | sh\""
-userDo "curl https://nixos.org/nix/install | sh"
+userDo $1 ". /home/$PROJECT_USER/.nix-profile/etc/profile.d/nix.sh"
 
-echo "userDo \". /home/$PROJECT_USER/.nix-profile/etc/profile.d/nix.sh\""
-userDo ". /home/$PROJECT_USER/.nix-profile/etc/profile.d/nix.sh"
+allCd $1 "$PROJECT_SIMPLICITY_DIR"
 
-echo "cd $PROJECT_SIMPLICITY_DIR"
-cd $PROJECT_SIMPLICITY_DIR
-
-echo "userDo \"nix-shell -p \"(import ./default.nix {}).haskellPackages.ghcWithPackages (pkgs: with pkgs; [Simplicity bech32])\"" 
-userDo "nix-shell -p \"(import ./default.nix {}).haskellPackages.ghcWithPackages (pkgs: with pkgs; [Simplicity bech32])\""
+userDo $1 "nix-shell -p \"(import ./default.nix {}).haskellPackages.ghcWithPackages (pkgs: with pkgs; [Simplicity bech32])\""
 
 echo "**************"
 echo "Elements"
 echo "**************"
 
-echo "cd $PROJECT_ELEMENTS_DIR"
-cd $PROJECT_ELEMENTS_DIR
+allCd $1 "$PROJECT_ELEMENTS_DIR"
 
-echo "userDo ./autogen.sh" 
-userDo ./autogen.sh
+userDo $1 "$PROJECT_ELEMENTS_DIR./autogen.sh"
 
-userDo ./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --disable-dependency-tracking --with-gui=no --disable-test --disable-bench >> $INSTALL_LOG_DIR/elements_congigure.log
+userDoLog $1  ./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --disable-dependency-tracking --with-gui=no --disable-test --disable-bench" "elements_congigure"
 
-echo "make >> $INSTALL_LOG_DIR/elements_make.log"
-make >> $INSTALL_LOG_DIR/element_make.log
-
-echo "make install >> $INSTALL_LOG_DIR/elements_make_install.log"
-make install >> $INSTALL_LOG_DIR/elements_make_install.log
-which elementsd
+suDoLog "make" "element_make"
+suDoLog "make install" "elements_make_install"
+suDo "which elementsd"
 
 echo "**************"
 echo "Personas"
 echo "**************"
-echo "cd $PROJECT_ELEMENTS_DIR/contrib/assets_tutorial"
-cd $PROJECT_ELEMENTS_DIR/contrib/assets_tutorial
+allCd $1 "$PROJECT_ELEMENTS_DIR/contrib/assets_tutorial"
 
-if [ -d "$USER_BITCOIN_DIR" ]
-then
-    echo "$USER_BITCOIN_DIR exist, rm"
-    rm -rf $USER_BITCOIN_DIR
-fi
-echo "mkdir $USER_BITCOIN_DIR" 
-mkdir $USER_BITCOIN_DIR
-echo "chmod 0755 $USER_BITCOIN_DIR && chown $PROJECT_USER $USER_BITCOIN_DIR"
-chmod 0755 $USER_BITCOIN_DIR && chown $PROJECT_USER $USER_BITCOIN_DIR
-cp ./bitcoin.conf $BITCOIN_DIR/bitcoin.conf
+userMkdir $1 "$USER_BITCOIN_DIR"
+userDo $1 "cp ./bitcoin.conf $BITCOIN_DIR/bitcoin.conf"
 
-if [ -d "$USER_ALICE_DIR" ]
-then
-    echo "$USER_ALICE_DIR exist, rm"
-    rm -rf $USER_ALICE_DIR
-fi
-echo "mkdir $USER_ALICE_DIR"
-mkdir $USER_ALICE_DIR
-echo "chmod 0755 $USER_ALICE_DIR && chown $PROJECT_USER $USER_ALICE_DIR"
-chmod 0755 $USER_ALICE_DIR && chown $PROJECT_USER $USER_ALICE_DIR
-cp ./elements1.conf $USER_ALICE_DIR/elements.conf
+userMkdir $1 "$USER_ALICE_DIR"
+userDo $1 "cp ./elements1.conf $USER_ALICE_DIR/elements.conf"
 
-if [ -d "$USER_BOB_DIR" ]
-then
-    echo "$USER_BOB_DIR exist, rm"
-    rm -rf $USER_BOB_DIR
-fi
-echo "mkdir $USER_BOB_DIR"
-mkdir $USER_BOB_DIR
-echo "chmod 0755 $USER_BOB_DIR && chown $PROJECT_USER $USER_BOB_DIR"
-chmod 0755 $USER_BOB_DIR && chown $PROJECT_USER $USER_BOB_DIR
-cp ./elements2.conf $USER_BOB_DIR/elements.conf
+userMkdir $1 "$USER_BOB_DIR"
+userDo $1 "cp ./elements2.conf $USER_BOB_DIR/elements.conf"
 
 echo "**************"
 echo "Start deamons"
 echo "**************"
-b-dae
-alice-dae
-bob-dae
+userDo $1 "b-dae"
+userDo $1 "alice-dae"
+userDo $1 "bob-dae"
 
 echo "**************"
 echo "Personas address"
 echo "**************"
-echo "userDo ALICE_MINER_ADDRESS=$(alice-cli getnewaddress)"
-userDo ALICE_MINER_ADDRESS=$(alice-cli getnewaddress)
+echo "# ALICE_MINER_ADDRESS=$(alice-cli getnewaddress)"
+ALICE_MINER_ADDRESS=$(alice-cli getnewaddress)
 echo "ALICE_MINER_ADDRESS=$ALICE_MINER_ADDRESS" >> $PROJECT_CONF
-echo "echo \"ALICE_MINER_ADDRESS = \"$ALICE_MINER_ADDRESS\"" >> $PROJECT_CONF
 
-echo "userDo ALICE_RECEIVER_ADDRESS=$(alice-cli getnewaddress)" 
-userDo ALICE_RECEIVER_ADDRESS=$(alice-cli getnewaddress)
+echo "# ALICE_RECEIVER_ADDRESS=$(alice-cli getnewaddress)"
+ALICE_RECEIVER_ADDRESS=$(alice-cli getnewaddress)
 echo "ALICE_RECEIVER_ADDRESS=$ALICE_RECEIVER_ADDRESS" >> $PROJECT_CONF
-echo "echo \"ALICE_RECEIVER_ADDRESS = \"$ALICE_RECEIVER_ADDRESS\"" >> $PROJECT_CONF
 
-echo "userDo BOB_RECEIVER_ADDRESS=$(bob-cli getnewaddress)"
-userDo BOB_RECEIVER_ADDRESS=$(bob-cli getnewaddress)
+echo "# BOB_RECEIVER_ADDRESS=$(bob-cli getnewaddress)"
+BOB_RECEIVER_ADDRESS=$(bob-cli getnewaddress)
 echo "BOB_RECEIVER_ADDRESS=$BOB_RECEIVER_ADDRESS" >> $PROJECT_CONF
-echo "echo \"BOB_RECEIVER_ADDRESS = \"$BOB_RECEIVER_ADDRESS\"" >> $PROJECT_CONF
 
-echo "ALICE_MINER_ADDRESS = $ALICE_MINER_ADDRESS"
-echo "ALICE_RECEIVER_ADDRESS = $ALICE_RECEIVER_ADDRESS"
-echo "BOB_RECEIVER_ADDRESS = $BOB_RECEIVER_ADDRESS"
+echo "# ALICE_MINER_ADDRESS = $ALICE_MINER_ADDRESS" 
+ALICE_MINER_ADDRESS = $ALICE_MINER_ADDRESS
+echo "ALICE_MINER_ADDRESS=$ALICE_MINER_ADDRESS" >> $PROJECT_CONF
 
 echo "**************"
 echo "chmod +x"
 echo "**************"
-echo "cd $INSTALL_DIR"
-cd $INSTALL_DIR
-echo "chmod +x $INSTALL_DIR/elementsProjectStart.sh"
-chmod +x $INSTALL_DIR/elementsProjectStart.sh
-echo "chmod +x $INSTALL_DIR/elementsProjectStop.sh"
-chmod +x $INSTALL_DIR/elementsProjectStop.sh
-echo "chmod +x $INSTALL_DIR/test_transaction_simple.sh"
-chmod +x $INSTALL_TEST_DIR/test_transaction_simple.sh
+allCd $1 "$INSTALL_DIR"
+chmodx "$INSTALL_DIR/elementsProjectStart.sh"
+chmodx "$INSTALL_DIR/elementsProjectStop.sh"
+chmodx "$INSTALL_DIR/../../test/test_transaction_simple.sh"
 
-echo "cd $INSTALL_LOG_DIR" 
-cd $INSTALL_LOG_DIR
-ls -al
+allCd $1 "$INSTALL_LOG_DIR"
+userDo $1 "ls -al"
+
