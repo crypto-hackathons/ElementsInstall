778fc2c82faa7255f03cb9e4fdb75c5ff9025ef5
crypto-hackathons <63258586+crypto-hackathons@users.noreply.github.com>
2020-06-01 12:06:46 +0200

    move to /opt

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 function sourceForUser()
 {	
 	PROJECT_CONF="../../conf/elementsProject.conf"
@@ -19,18 +18,18 @@ function sourceForUser()
 	echo "# chmod +x $USER_CONF" 
 	chmod +x $USER_CONF
 }
-echo ""
-echo ""
-echo ""
-echo "**************"
-echo "The full install take around 2h"
-echo "**************"
-echo ""
-echo ""
-echo ""   	
-echo "**************"
-echo "Configure"
-echo "**************"
+echo "# "
+echo "# "
+echo "# "
+echo "# **************"
+echo "# The full install take around 2h"
+echo "# **************"
+echo "# "
+echo "# "
+echo "# "   	
+echo "# **************"
+echo "# Configure"
+echo "# **************"
 USER=$USER
 APT="yes"
 BERKELEY_DB="yes"
@@ -62,25 +61,26 @@ while getopts u:a:k:b:s:n:e:p:v:j:g: option
 
 if [ $USER == "root" ]
    then
-   		echo "USER must not be root"
+   		echo "# echo \"USER must not be root\""
    		exit
 fi
 echo "# ./install.sh -u ncantu -a $USER -k $APT -b $BERKELEY_DB -s $BITCOIN -n $NIX -e $ELEMENTS -p $PERSONAS -v $ENVIRONNEMENT -j =$NODES -w $WALLET"
-echo "USER=$USER"
-echo "APT=$APT"
-echo "BERKELEY_DB=$BERKELEY_DB"
-echo "BITCOIN=$BITCOIN"
-echo "NIX=$NIX"
-echo "ELEMENTS=$ELEMENTS"
-echo "PERSONAS=$PERSONAS"
-echo "ENVIRONNEMENT=$ENVIRONNEMENT"
-echo "WALLET=$WALLET"
-
+echo "# USER=$USER"
+echo "# APT=$APT"
+echo "# BERKELEY_DB=$BERKELEY_DB"
+echo "# BITCOIN=$BITCOIN"
+echo "# NIX=$NIX"
+echo "# ELEMENTS=$ELEMENTS"
+echo "# PERSONAS=$PERSONAS"
+echo "# ENVIRONNEMENT=$ENVIRONNEMENT"
+echo "# WALLET=$WALLET"
+echo "# HERE=`pwd`"
 HERE=`pwd`
-sourceForUser $USER "root" $ENVIRONNEMENT
-su $USER -c "cd $HERE && source functions.sh && sourceForUser $USER $USER $ENVIRONNEMENT"
 
-echo "# echo \"run with $USER\""
+echo "# sourceForUser $USER "root" $ENVIRONNEMENT"
+sourceForUser $USER "root" $ENVIRONNEMENT
+echo "$ cd $HERE && source functions.sh && sourceForUser $USER $USER $ENVIRONNEMENT"
+su $USER -c "cd $HERE && source functions.sh && sourceForUser $USER $USER $ENVIRONNEMENT"
 
 if [ -d "$PROJECT_DIR" ]; then
 	echo "# echo \"$PROJECT_DIR exist\""
@@ -89,18 +89,27 @@ else
    mkdir $PROJECT_DIR
    echo "# chmod 0755 $PROJECT_DIR && chown $USER $PROJECT_DIR"
    chmod 0755 $PROJECT_DIR && chown $USER $PROJECT_DIR
-fi
+fi   
+
+echo "# echo \"Move to $PROJECT_DIR""
+userGitClone $USER "$PROJECT_DIR" "https://github.com/crypto-hackathons/ElementsInstall.git" "ElementProject_install" "$INSTALL_DIR"
+echo "# chmod 0755 $PROJECT_DIR && chown $USER $PROJECT_DIR"
+chmod 0755 $INSTALL_DIR && chown $USER $INSTALL_DIR
+echo "# chmod 0755 $PROJECT_DIR && chown $USER $PROJECT_DIR"
+chmod 0755 $INSTALL_DIR && chown $USER $INSTALL_DIR
+echo "# rm -rf $HERE"
+rm -rf $HERE
 
 if [ "$APT" == "yes" ]
    then 
-   		echo ""
-   		echo ""
-   		echo ""
-		echo "**************"
-		echo "Apt install"
-		echo "The full install take around 2h"
-		echo "This part take around 10min"
-		echo "**************"
+   		echo "# "
+   		echo "# "
+   		echo "# "
+		echo "# **************"
+		echo "# Apt install"
+		echo "# The full install take around 2h"
+		echo "# This part take around 10min"
+		echo "# **************"
 		suDoLog $INSTALL_DIR "apt-get install -y apt-utils" "apt_install"
 		suDoLog $INSTALL_DIR "apt-get update -y" "apt_update" "apt_update"
 		suDoLog $INSTALL_DIR "apt-get upgrade -y" "apt_upgrade" "apt_upgrade"
@@ -109,28 +118,28 @@ if [ "$APT" == "yes" ]
 fi
 if [ "$BERKELEY_DB" == "yes" ]
    then    		
-   		echo ""
-   		echo ""
-   		echo ""
-		echo "**************"
-		echo "Berkeley DB install"
-		echo "The full install take around 2h"
-		echo "This part take around 5min"
-		echo "**************"
+   		echo "# "
+   		echo "# "
+   		echo "# "
+		echo "# **************"
+		echo "# Berkeley DB install"
+		echo "# The full install take around 2h"
+		echo "# This part take around 5min"
+		echo "# **************"
 		userMkdir $USER "$PROJECT_DIR"
 		userGitClone $USER "$PROJECT_DIR" "https://github.com/ElementsProject/elements.git" "elements_gitClone" "$PROJECT_ELEMENTS_DIR"
 		suDoLog "$PROJECT_ELEMENTS_DIR" "$PROJECT_ELEMENTS_DIR/contrib/install_db4.sh $DB4_INSTALL_PATH" "db4_install"
 fi
 if [ "$BITCOIN" == "yes" ]
    then    		
-   		echo ""
-   		echo ""
-   		echo ""
-		echo "**************"
-		echo "Bitcoin install"
-		echo "The full install take around 2h"
-		echo "This part take around 40min"
-		echo "**************"		
+   		echo "# "
+   		echo "# "
+   		echo "# "
+		echo "# **************"
+		echo "# Bitcoin install"
+		echo "# The full install take around 2h"
+		echo "# This part take around 40min"
+		echo "# **************"		
 		userGitClone $USER "$PROJECT_DIR" "https://github.com/roconnor-blockstream/bitcoin.git" "bitcoin_install" "$PROJECT_BITCOIN_DIR"
 		userGitChekout $USER "$PROJECT_BITCOIN_DIR" $PROJECT_BITCOIN_BRANCH
 		userDoLog $USER "$PROJECT_BITCOIN_DIR" "$PROJECT_BITCOIN_DIR/./autogen.sh" "bitcoin_install"
@@ -139,14 +148,14 @@ if [ "$BITCOIN" == "yes" ]
 fi
 if [ "$SIMPLICITY" == "yes" ]
    then   
-   		echo ""
-   		echo ""
-   		echo ""
-		echo "**************"
-		echo "Simplicity install"
-		echo "The full install take around 2h"
-		echo "This part take around 5min"
-		echo "**************"
+   		echo "# "
+   		echo "# "
+   		echo "# "
+		echo "# **************"
+		echo "# Simplicity install"
+		echo "# The full install take around 2h"
+		echo "# This part take around 5min"
+		echo "# **************"
 		userGitClone $USER "$PROJECT_DIR" "https://github.com/ElementsProject/simplicity.git" "simplicity_install" "$PROJECT_SIMPLICITY_DIR"
 		userGitChekout $USER "$PROJECT_SIMPLICITY_DIR" "$PROJECT_SIMPLICITY_BRANCH"
 		userDoLog $USER "$PROJECT_DIR" "cabal update" "simplicity_install"		
@@ -160,14 +169,14 @@ if [ "$SIMPLICITY" == "yes" ]
 fi
 if [ "$NIX" == "yes" ]
    then
-   		echo ""
-   		echo ""
-   		echo ""
-		echo "**************"
-		echo "Nix install"
-		echo "The full install take around 2h"
-		echo "This part take around 5min"
-		echo "**************"
+   		echo "# "
+   		echo "# "
+   		echo "# "
+		echo "# **************"
+		echo "# Nix install"
+		echo "# The full install take around 2h"
+		echo "# This part take around 5min"
+		echo "# **************"
 		userMkdir $USER "$PROJECT_NIX_DIR"		
 		userDoLog $USER "$PROJECT_DIR" "curl https://nixos.org/nix/install | sh"		
 		userDo $USER "$PROJECT_DIR" ". /home/$PROJECT_USER/.nix-profile/etc/profile.d/nix.sh"		
@@ -176,42 +185,36 @@ if [ "$NIX" == "yes" ]
 fi
 if [ "$ELEMENTS" == "yes" ]
    then
-   		echo ""
-   		echo ""
-   		echo ""
-		echo "**************"
-		echo "Elements install"
-		echo "This part take around 40min"
-		echo "**************"	
+   		echo "# "
+   		echo "# "
+   		echo "# "
+		echo "# **************"
+		echo "# Elements install"
+		echo "# This part take around 40min"
+		echo "# **************"	
 		userGitChekout $USER "$PROJECT_ELEMENTS_DIR" $PROJECT_ELEMENTS_BRANCH			
 		userDo $USER "$PROJECT_ELEMENTS_DIR" "$PROJECT_ELEMENTS_DIR/./autogen.sh"		
 		userDoLog $USER "$PROJECT_ELEMENTS_DIR" "$ELEMENTS_CONFIGURE" "elements_install"
-		suDoLog  "$PROJECT_ELEMENTS_DIR" "make" "elements_install"
-		suDoLog  "$PROJECT_ELEMENTS_DIR" "make install" "elements_install"
+		suDoLog "$PROJECT_ELEMENTS_DIR" "make" "elements_install"
+		suDoLog "$PROJECT_ELEMENTS_DIR" "make install" "elements_install"
 fi
 if [ "$PERSONAS" == "yes" ]
    then
-   		echo ""
-   		echo ""
-   		echo ""
-		echo "**************"
-		echo "Personas install"
-		echo "The full install take around 2h"
-		echo "This part take around 1min"
-		echo "**************"		
+   		echo "# "
+   		echo "# "
+   		echo "# "
+		echo "# **************"
+		echo "# Personas install"
+		echo "# The full install take around 2h"
+		echo "# This part take around 1min"
+		echo "# **************"		
 		userMkdir $USER "$USER_BITCOIN_DIR"
-		
-		BC="regtest=$PROJECT_BITCOIN_REGTEST\ndaemon=$PROJECT_BITCOIN_DAEMON\ntxindex=$PROJECT_BITCOIN_TXINDEX\nregtest.rpcport=$PROJECT_BITCOIN_REGTEST_RPC_PORT\nregtest.port=$PROJECT_BITCOIN_REGTEST_PORT\nrpcuser=$PROJECT_BITCOIN_REGTEST_RPC_USER\nrpcpassword=password$PROJECT_BITCOIN_REGTEST_RPC_PASSWORD\n"
+				BC="regtest=$PROJECT_BITCOIN_REGTEST\ndaemon=$PROJECT_BITCOIN_DAEMON\ntxindex=$PROJECT_BITCOIN_TXINDEX\nregtest.rpcport=$PROJECT_BITCOIN_REGTEST_RPC_PORT\nregtest.port=$PROJECT_BITCOIN_REGTEST_PORT\nrpcuser=$PROJECT_BITCOIN_REGTEST_RPC_USER\nrpcpassword=password$PROJECT_BITCOIN_REGTEST_RPC_PASSWORD\n"
 
-		echo "# echo -e $BC >> $USER_BITCOIN_CONF"
-		echo -e $BC >> $USER_BITCOIN_CONF
-
-		echo "# chmod 0755 $USER_BITCOIN_CONF && chown $USER $USER_BITCOIN_CONF"
-		chmod 0755 $USER_BITCOIN_CONF && chown $USER $USER_BITCOIN_CONF
-		echo cat $USER_BITCOIN_CONF
-		
-		echo "# nohup $PROJECT_BITCOIN_DIR/src/./bitcoind -datadir=$USER_BITCOIN_DIR &>/dev/null &"
-		btcd
+		suDoLog "$PROJECT_DIR" "echo -e $BC >> $USER_BITCOIN_CONF" "PersonasInstall"
+        suDoLog "$PROJECT_DIR" "chmod 0755 $USER_BITCOIN_CONF && chown $USER $USER_BITCOIN_CONF" "PersonasInstall"
+        suDoLog "$PROJECT_DIR" "echo cat $USER_BITCOIN_CONF" "PersonasInstall"
+		suDoLog "$PROJECT_DIR" "btcd" "PersonasInstall"
 		
 		echo "# personaCreate $PROJECT_ELEMENTS_REGTEST_RPC_USER_ALICE $PROJECT_ELEMENTS_REGTEST_RPC_PASSWORD_ALICE $PROJECT_ELEMENTS_REGTEST_RPC_PORT_ALICE $PROJECT_ELEMENTS_REGTEST_PORT_ALICE $PROJECT_ELEMENTS_REGTEST_PORT_LOCAL_ALICE $PROJECT_ELEMENTS_INITIAL_FREE_COINS_ALICE $USER_ALICE_NAME"
 		personaCreate $PROJECT_ELEMENTS_REGTEST_RPC_USER_ALICE $PROJECT_ELEMENTS_REGTEST_RPC_PASSWORD_ALICE $PROJECT_ELEMENTS_REGTEST_RPC_PORT_ALICE $PROJECT_ELEMENTS_REGTEST_PORT_ALICE $PROJECT_ELEMENTS_REGTEST_PORT_LOCAL_ALICE $PROJECT_ELEMENTS_INITIAL_FREE_COINS_ALICE $USER_ALICE_NAME
@@ -219,52 +222,47 @@ if [ "$PERSONAS" == "yes" ]
 		echo "# personaCreate $PROJECT_ELEMENTS_REGTEST_RPC_USER_BOB $PROJECT_ELEMENTS_REGTEST_RPC_PASSWORD_BOB $PROJECT_ELEMENTS_REGTEST_RPC_PORT_BOB $PROJECT_ELEMENTS_REGTEST_PORT_BOB $PROJECT_ELEMENTS_REGTEST_PORT_LOCAL_BOB $PROJECT_ELEMENTS_INITIAL_FREE_COINS_BOB $USER_BOB_NAME"	
 		personaCreate $PROJECT_ELEMENTS_REGTEST_RPC_USER_BOB $PROJECT_ELEMENTS_REGTEST_RPC_PASSWORD_BOB $PROJECT_ELEMENTS_REGTEST_RPC_PORT_BOB $PROJECT_ELEMENTS_REGTEST_PORT_BOB $PROJECT_ELEMENTS_REGTEST_PORT_LOCAL_BOB $PROJECT_ELEMENTS_INITIAL_FREE_COINS_BOB $USER_BOB_NAME
 		
-		echo 
+		echo "# personaAddressAdd \"ALICE\" \"MINER\""
 		personaAddressAdd "ALICE" "MINER"
 			
-		chmodx "$INSTALL_DIR/../elementsProjectStart.sh"
-		chmodx "$INSTALL_DIR/../elementsProjectStop.sh"
-		chmodx "$INSTALL_DIR/../../test/test_transaction_simple.sh"
+        suDoLog "$PROJECT_DIR" "chmodx \"$INSTALL_DIR/../elementsProjectStart.sh\"" "PersonasInstall"
+        suDoLog "$PROJECT_DIR" "chmodx \"$INSTALL_DIR/../elementsProjectStop.sh\"" "PersonasInstall"
+        suDoLog "$PROJECT_DIR" "chmodx \"$INSTALL_DIR/../test/test_transaction_simple.sh\"" "PersonasInstall"
 fi
 if [ "$NODE" == "yes" ]	
    then   
-	echo ""
-	echo ""
-	echo ""
-	echo "**************"
-	echo "Node install"
-	echo "This part take around 2min"
-	echo "**************"
-    curl -sL https://deb.nodesource.com/setup_14.x | bash -
-   	apt-get install -y nodejs
-   	npm install -g npm@latest
+	echo "# "
+	echo "# "
+	echo "# "
+	echo "# **************"
+	echo "# Node install"
+	echo "# This part take around 2min"
+	echo "# **************"
+    suDoLog "$PROJECT_DIR" "curl -sL $NODE_INSTALL_URL | bash -" "nodeInstall"
+    suDoLog "$PROJECT_DIR" "apt-get install -y nodejs" "nodeInstall"
+    suDoLog "$PROJECT_DIR" "npm install -g npm@latest" "nodeInstall"
 fi
 if [ "$WALLET" == "yes" ]
    then   
-	echo ""
-	echo ""
-	echo ""
-	echo "**************"
-	echo "Wallet app framework install"
-	echo "This part take around 2min"
-	echo "**************"
-   	git clone git://github.com/bcoin-org/bcoin.git
-	cd bcoin
-	npm rebuild
-	npm install --global
+	echo "# "
+	echo "# "
+	echo "# "
+	echo "# **************"
+	echo "# Bcoin install"
+	echo "# This part take around 2min"
+	echo "# **************"
+    userGitClone $USER "$PROJECT_DIR" "$BCOIN_GIT" "bcoinInstall" "$BCOIN_DIR"
+	suDoLog "$BCOIN_DIR" "npm rebuild" "bcoinInstall"
+    suDoLog "$BCOIN_DIR" "npm install --global" "bcoinInstall"
 	# bcoin --http-host=0.0.0.0 --api-key $USER --daemon
 fi
 exit
-echo ""
-echo ""
-echo ""
-cd $INSTALL_DIR
-cat ../../conf/elementsProject.conf >> $INSTALL_LOG_DIR/result.log
-cat /home/$USER/.elementsProject_$USER_$USER.conf >> $INSTALL_LOG_DIR/result.log
-cat USER_BITCOIN_CONF >> $INSTALL_LOG_DIR/result.log
-cat /home/$PROJECT_USER/ALICEData/elements.conf >> $INSTALL_LOG_DIR/result.log
-cat /home/$PROJECT_USER/BOBData/elements.conf >> $INSTALL_LOG_DIR/result.log
-ls $INSTALL_LOG_DIR -al >> $INSTALL_DIR/result.log
-
-echo "# cat $INSTALL_LOG_DIR/result.log"
-cat $INSTALL_LOG_DIR/result.log
+echo "# "
+echo "# "
+echo "# "
+suDo "$INSTALL_DIR" "cat ../../conf/elementsProject.conf >> $INSTALL_LOG_DIR/result.log" "resultInstall"
+suDo "$INSTALL_DIR" "cat /home/$USER/.elementsProject_$USER_$USER.conf >> $INSTALL_LOG_DIR/result.log" "resultInstall"
+suDo "$INSTALL_DIR" "cat USER_BITCOIN_CONF >> $INSTALL_LOG_DIR/result.log" "resultInstall"
+suDo "$INSTALL_DIR" "cat /home/$PROJECT_USER/ALICEData/elements.conf >> $INSTALL_LOG_DIR/result.log" "resultInstall"
+suDo "$INSTALL_DIR" "cat /home/$PROJECT_USER/BOBData/elements.conf >> $INSTALL_LOG_DIR/result.log" "resultInstall"
+suDo "$INSTALL_LOG_DIR" "echo "ls $INSTALL_LOG_DIR -al >> $INSTALL_DIR/result.log"
